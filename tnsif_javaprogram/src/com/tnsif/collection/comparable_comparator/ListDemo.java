57475afd25c093209ed7fb634aaddf0f03a83b7c
vanitha1402 <vanithavengatesan4@gmail.com>
2025-07-17 16:18:27 +0530

    commit

---
+++ tnsif_javaprogram/src/com/tnsif/collection/comparable_comparator/ListDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.collection.comparable_comparator;
+
+public class ListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ tnsif_javaprogram/src/com/tnsif/collection/comparable_comparator/Person.java
@@ -0,0 +1,10 @@
+package com.tnsif.collection.comparable_comparator;
+
+public class Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
